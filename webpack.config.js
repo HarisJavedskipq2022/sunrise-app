b54a4b35e60118c1e3fd98989e130f2d10965678
Haris <haris.javed.skipq@gmail.com>
2023-05-31 12:45:50 +0500

    Revert "Update webpack.config.js"
    This reverts commit 38947b41eca7e7a1df7e0cf4a70fcc211990c465.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-export const module = {
-	rules: [
-		{
-			test: /\.(js|jsx)$/,
-			exclude: /node_modules/,
-			use: {
-				loader: "babel-loader",
-			},
-		},
-		{
-			test: /\.html$/,
-			use: [
-				{
-					loader: "html-loader",
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
 				},
-			],
-		},
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: "html-loader",
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html",
+		}),
 	],
 };
-export const plugins = [
-	new HtmlWebpackPlugin({
-		template: "./src/index.html",
-		filename: "./index.html",
-	}),
-];
