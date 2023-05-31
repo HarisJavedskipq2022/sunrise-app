38947b41eca7e7a1df7e0cf4a70fcc211990c465
Haris <haris.javed.skipq@gmail.com>
2023-05-31 12:42:59 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,27 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
+export const module = {
+	rules: [
+		{
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			use: {
+				loader: "babel-loader",
+			},
+		},
+		{
+			test: /\.html$/,
+			use: [
+				{
+					loader: "html-loader",
 				},
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: "html-loader",
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html",
-			filename: "./index.html",
-		}),
+			],
+		},
 	],
 };
+export const plugins = [
+	new HtmlWebpackPlugin({
+		template: "./src/index.html",
+		filename: "./index.html",
+	}),
+];
